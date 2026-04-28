092cc3e4f5dc9ed9bf9367434752da2a0f1c25f9
BeastAxeOne <albabalbab2023@gmail.com>
2026-04-29 04:06:27 +0600

    Update PostItem.jsx

--- app/src/pages/PostItem.jsx
+++ app/src/pages/PostItem.jsx
@@ -118,7 +118,7 @@ export function PostItem() {
     <>
       <Header />
 
-      <div className="container section" style={{maxwidth="600px", width="70%"}}>
+      <div className="post-container">
 
         <h1>Post Found Item</h1>
 
